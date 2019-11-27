561f8c29e4d7191ba8e778912941f859f867a605
Ricardo Pramana Suranta <ricardo.pramana@gmail.com>
2019-11-27 13:05:04 -0800

    Update footer_menu to display the input image temporarily

--- views/public/javascripts/footer_menu.js
+++ views/public/javascripts/footer_menu.js
@@ -4,6 +4,9 @@ function bindFooterButtons() {
     bindFooterHomeButton()
     bindFooterProfileButton()
     bindFooterTakePictureButton()
+
+    hideImageInputDisplay()
+    observeTakePictureInput()
 }
 
 function bindFooterHomeButton() {
@@ -15,8 +18,40 @@ function bindFooterProfileButton() {
 }
 
 function bindFooterTakePictureButton() {
-    
+
     $("#footer_btn_take_picture").click( () => {
         $("#footer_take_picture_input").trigger("click")
     })
+}
+
+function hideImageInputDisplay() {
+    // TODO: Update this implementation to show the full file after the analysis page is ready.
+    $("#image_input_display").hide()
+}
+
+function observeTakePictureInput() {
+
+    $("#footer_take_picture_input").change(function (){
+        // TODO: Update this implementation to show the full file after the analysis page is ready.
+        displayImage(this)
+    })
+
+}
+
+function displayImage(input) {
+
+    var fileAvailable = (input.files != null) && (input.files[0] != null)
+
+    if (!fileAvailable) {
+        return
+    }
+
+    var fileReader = new FileReader()
+
+    fileReader.onload = (file) => {
+        $("#image_input_display").attr("src", file.target.result)
+        $("#image_input_display").show()
+    }
+
+    fileReader.readAsDataURL(input.files[0])
 }
