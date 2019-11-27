8bb841cdd09528aedf5522d8b34959dcf68e18e2
jmerlinz <nieng123@hotmail.com>
2019-11-26 16:10:36 -0800

    Push style

--- routes/SRIDRoute.js
+++ routes/SRIDRoute.js
@@ -10,5 +10,17 @@ module.exports = class FSERoute {
         app.get('/', (req, res) => {
             res.sendFile(path.resolve(__dirname + '/../views/public/index.html'));
         });
+
+        app.get('/styleguide', (req, res) => {
+            res.sendFile(path.resolve(__dirname + '/../styleguide/index.html'));
+        });
+
+        app.get('/leader', (req, res) => {
+            res.sendFile(path.resolve(__dirname + '/../views/public/leaderboard.html'));
+        });
+
+        app.get('/footer', (req, res) => {
+            res.sendFile(path.resolve(__dirname + '/../views/public/footer_menu.html'));
+        });
     }
 }
