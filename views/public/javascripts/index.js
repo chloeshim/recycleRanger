de334f2980474e19c22922e33477d65f7b557019
Chloe Shim <woojoo1210@gmail.com>
2019-11-28 11:37:51 -0800

    header logo added

--- views/public/javascripts/index.js
+++ views/public/javascripts/index.js
@@ -3,5 +3,8 @@ $(function(){
     $("#footerMenu").load("footer_menu.html", () => {
         bindFooterButtons()
     })
-    
+
+    $("#daily_lesson").click(function() {
+        $(location).attr('href', '/lesson.html');
+    });
 })
