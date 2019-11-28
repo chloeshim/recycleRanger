1a33427b0c443f5bf2ad171b5bae98610cab010a
Chloe Shim <woojoo1210@gmail.com>
2019-11-28 13:39:28 -0800

    daily lesson page finished

--- views/public/javascripts/index.js
+++ views/public/javascripts/index.js
@@ -7,4 +7,12 @@ $(function(){
     $("#daily_lesson").click(function() {
         $(location).attr('href', '/lesson.html');
     });
+
+    $("#today_quiz").click(function() {
+        $(location).attr('href', '/quiz.html');
+    });
+
+    $("#lesson_btn").click(function() {
+        $(location).attr('href', '/index.html');
+    });
 })
