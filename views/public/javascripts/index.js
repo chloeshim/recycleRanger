4892dd73d2976a4d4a9ecdba27e90a15eb4c9f80
ErguiXie <ergui.xie@sv.cmu.edu>
2019-12-02 02:09:48 -0800

    adopted profile page style to index page; added socket to update heart numbers. First draft

--- views/public/javascripts/index.js
+++ views/public/javascripts/index.js
@@ -1,9 +1,15 @@
 $(function(){
-      
+
+    var socket = io();
+    socket.on("currentScore", (currentScore)=>{
+        console.log(currentScore);
+        $("#heart_number").text(currentScore.toString());
+    })
+
     $("#footerMenu").load("footer_menu.html", () => {
 
         // TODO: This one 1 second delay is needed to wait for the whole page to be rendered. Revisit if there's a cleaner way to do this later.
-        setTimeout(bindFooterButtons, 1000)        
+        setTimeout(bindFooterButtons, 1000)
     })
 
     $("#daily_lesson").click(function() {
