e5ef8b1c6dc5bd22d85b37eac6ac81466d03fa80
jmerlinz <nieng123@hotmail.com>
2019-12-02 02:24:35 -0800

    Adjust style for leaderboard, cheerpeer and move sasha heart data

--- views/public/javascripts/friendsData.js
+++ views/public/javascripts/friendsData.js
@@ -1,4 +1,3 @@
 let friendList = {};
-const sashaHeart = 1197;
 friendList.friends = ["Panu", "Eric", "Luca", "Ricardo", "Chloe"];
 friendList.friendHearts = [542, 250, 345, 428, 666];
--- views/public/javascripts/leaderboard.js
+++ views/public/javascripts/leaderboard.js
@@ -1,6 +1,6 @@
 $(() => {
     $("#allFriends").text(friendList.friends.length);
     for(let i = 0;i< friendList.friends.length; i++){
-        $("#leaderboard_content").append('<h6><img src="images/user.png" class="frieng_img" ><div style="width:58px; display: inline-block; ">'+ friendList.friends[i] +'</div><img src="images/red-heart.png" height="30" width="30"> </img> '+ friendList.friendHearts[i] +'<a href="cheer_completed?id='+ i +'"><img src="images/cheer_icon.png" class="cheer_img" ></a></h6>');
+        $("#leaderboard_content").append('<h6 style="margin-bottom:18px;"><img src="images/user.png" class="frieng_img" ><div style="width:98px; display: inline-block; ">'+ friendList.friends[i] +'</div><img src="images/red-heart.png" height="30" width="30"> </img> '+ friendList.friendHearts[i] +'<a href="cheer_completed?id='+ i +'"><div class="cheer_item"><img src="images/cheer_icon.png" class="cheer_img" ><span class="caption">Cheer</span></div></a></h6>');
     }
 })
